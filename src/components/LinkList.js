f3a34f1ac0ab63687167fc9d64d467edba100ce4
Spencer Mefford <spencermefford@gmail.com>
2018-04-02 13:11:15 -0600

    Update votes

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -4,6 +4,15 @@ import React, { Component } from 'react'
 import Link from './Link'
 
 class LinkList extends Component {
+  _updateCacheAfterVote = (store, createVote, linkId) => {
+    const data = store.readQuery({ query: FEED_QUERY })
+
+    const votedLink = data.feed.links.find(link => link.id === linkId)
+    votedLink.votes = createVote.link.votes
+
+    store.writeQuery({ query: FEED_QUERY, data })
+  }
+
   render() {
     if (this.props.feedQuery && this.props.feedQuery.loading) {
       return <div>Loading</div>
@@ -18,7 +27,7 @@ class LinkList extends Component {
     return (
       <div>
         {linksToRender.map((link, index) => (
-          <Link key={link.id} index={index} link={link} />
+          <Link key={link.id} updateStoreAfterVote={this._updateCacheAfterVote} index={index} link={link}/>
         ))}
       </div>
     )
